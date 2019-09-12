ca2ea113e46bed72f3919b92466fb2e59c973661
ZenkakuHiragana <daiko1997@yahoo.co.jp>
2019-09-12 23:07:01 +0900

    Hat Painterとの競合の解決

--- lua/matproxy/weapon_splatooncolor.lua
+++ lua/matproxy/weapon_splatooncolor.lua
@@ -1,5 +1,5 @@
 
---Team Fortress 2 is required.
+-- Team Fortress 2 is required.
 if not IsMounted "tf" then return end
 
 local OldItemTintBind
@@ -8,17 +8,18 @@ local function ItemTintInit(self, mat, values)
 end
 
 for _, a in ipairs(engine.GetAddons()) do
-	if a.wsid ~= 135491961 then continue end
-	include "matproxy/tf2itempaint.lua" -- Hat Painter & Crit Glow Tools conflicts
-	ItemTintInit = matproxy.ProxyList.ItemTintColor.init -- So take some workaround
-	OldItemTintBind = matproxy.ProxyList.ItemTintColor.bind
+	if a.wsid == "135491961" then
+		include "matproxy/tf2itempaint.lua" -- Hat Painter & Crit Glow Tools conflicts
+		ItemTintInit = matproxy.ProxyList.ItemTintColor.init -- So take some workaround
+		OldItemTintBind = matproxy.ProxyList.ItemTintColor.bind
+	end
 end
 
 local function ItemTintBind(self, mat, ent)
 	if not IsValid(ent) then return end
 	if isfunction(ent.GetInkColorProxy) and isvector(ent:GetInkColorProxy()) then
-		--If the target ent has a function called GetInkColorProxy then use that
-		--The function SHOULD return a Vector with the chosen ink color.
+		-- If the target ent has a function called GetInkColorProxy then use that
+		-- The function SHOULD return a Vector with the chosen ink color.
 		mat:SetVector(self.ResultTo, ent:GetInkColorProxy())
 	elseif OldItemTintBind then
 		return OldItemTintBind(self, mat, ent)
